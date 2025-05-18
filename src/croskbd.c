1bae00283f9edfca676a30a77013c08416df7f83
WeirdTreeThing <bradyn127@protonmail.com>
2025-05-18 16:34:26 -0400

    oops

--- src/croskbd.c
+++ src/croskbd.c
@@ -20,21 +20,17 @@ UInputDevice udev = {
 	.fd = -1
 };
 
-int keyboard_fd = -1;
-int uinput_fd = 0;
-char keyboard_ev_name[10] = { 0 };
-
 int main(int argc, char** argv) {
 	scan_input_devices(&kdev, &tdev);
 
-	if (keyboard_fd < 0) {
+	if (kdev.fd < 0) {
 		printf("Failed to find keyboard device\n");
 		return(1);
 	}
 
 	uinput_init(&udev);
 
-	if (uinput_fd < 0) {
+	if (udev.fd < 0) {
 		printf("Failed to create virtual keyboard\n");
 		return 1;
 	}
