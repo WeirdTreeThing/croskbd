4c50b294df1428dcbc66a30f2da42f7971c93dc3
WeirdTreeThing <bradyn127@protonmail.com>
2025-05-20 19:34:49 -0400

    Decode vivaldi data

--- src/vivaldi.c
+++ src/vivaldi.c
@@ -1,7 +1,27 @@
 #include <croskbd.h>
 #include <fcntl.h>
+#include <linux/input.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
 #include <unistd.h>
+
+int convert_scancode_to_keycode(int fd, int scancode) {
+  struct input_keymap_entry entry = {
+      .flags = 0,
+      .len = sizeof(scancode),
+      .keycode = 0,
+  };
+
+  int ret = ioctl(fd, EVIOCGKEYCODE_V2, &entry);
+  if (ret < 0) {
+    perror("EVIOCGKEYCODE_V2");
+    return 0;
+  }
+
+  return entry.keycode;
+}
 
 int load_kb_vivaldi_data(KeyboardDevice *kdev) {
   char data_path[100];
@@ -15,10 +35,14 @@ int load_kb_vivaldi_data(KeyboardDevice *kdev) {
     perror("open vivaldi data");
     return 0;
   }
-
   read(fd, function_row_physmap, sizeof(function_row_physmap));
 
-  printf("physmap: %s\n", function_row_physmap);
+  char *tok = strtok(function_row_physmap, " ");
+  while (tok != NULL) {
+    int scancode = strtol(tok, NULL, 16);
+    printf("keycode: %d\n", convert_scancode_to_keycode(kdev->fd, scancode));
+    tok = strtok(NULL, " ");
+  }
 
   return 1;
 }
